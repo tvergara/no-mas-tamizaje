d6da4916bf65c98b6608fbcc3be70f5f6efccf86
Tomás Vergara <tomvergara@uc.cl>
2022-03-07 21:31:15 -0300

    feat(admin): add enqueue daily intents button

--- app/admin/fill_screening_form_intents.rb
+++ app/admin/fill_screening_form_intents.rb
@@ -10,4 +10,13 @@ ActiveAdmin.register FillScreeningFormIntent do
     column :error_category
     column :created_at
   end
+
+  action_item only: :index do
+    link_to 'Enqueue Daily Intents', enqueue_daily_intents_admin_fill_screening_form_intents_path, method: :post
+  end
+
+  collection_action :enqueue_daily_intents, method: :post do
+    EnqueueDailyScrapingJob.perform_later
+    redirect_to collection_path, notice: 'Daily intents enqueued'
+  end
 end
