96885ab50d9d0a94996c65fa532f933c607898c3
Tomás Vergara <tomvergara@uc.cl>
2022-03-07 21:11:38 -0300

    feat(): add waiting time between form attempts

--- app/jobs/enqueue_daily_scraping_job.rb
+++ app/jobs/enqueue_daily_scraping_job.rb
@@ -1,7 +1,11 @@
 class EnqueueDailyScrapingJob < ApplicationJob
+  WAITING_SECONDS = ENV.fetch('SCRAPE_WAITING_SECONDS', 10).to_i
+
   def perform
+    current_wait = 0
     User.active.scrapes_today.each do |user|
-      ScrapeScreeningFormJob.perform_later(user.id)
+      ScrapeScreeningFormJob.set(wait: current_wait.seconds).perform_later(user.id)
+      current_wait += WAITING_SECONDS
     end
   end
 end
